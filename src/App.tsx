82bd9d04d61d98d151e36e2729100ac13c6fde65
pswu11 <pswu@protonmail.com>
2023-06-21 12:16:44 +0200

    use data loader and frog-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-
 import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Blog } from "./pages/Blog"
 import { Projects } from "./pages/Projects"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { RootLayout } from "./layouts/Root"
+
+async function expLoader() {
+  try {
+    const response = await fetch(
+      "http://209.38.197.154:8000/projects/e4e6a7de-c29c-41d5-b910-66ebe9e9d5af/experience"
+    )
+    if (!response.ok) {
+      throw Error(`Error with status: ${response.status}`)
+    } else {
+      const data = await response.json()
+      return data
+    }
+  } catch (err) {
+    // Different error handling approaches for real-world use cases:
+    // 1. toastify to show a error notification
+    // 2. pass a parem and listen to it on the about page, and handle it there
+    // 3. redirect to an error page depending on the error
+    // But for personal website, this data is most likely hosted on the same server.
+  }
+}
+
 
 const router = createBrowserRouter([
   {
@@ -18,6 +38,7 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+        loader: expLoader
       },
       {
         path: "blog",
@@ -27,15 +48,12 @@ const router = createBrowserRouter([
         path: "projects",
         element: <Projects />,
       },
-    ]
+    ],
   },
 ])
 
 function App() {
-  return (
-    <RouterProvider router={router}/>
-    
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,133 +1,23 @@
 import { useState } from "react"
+import { useLoaderData } from "react-router-dom"
 import Experience from "../components/experience/Experience"
 import Summary from "../components/experience/Summary"
 import Tags from "../components/experience/Tags"
 
-const allExperiences = [
-  {
-    type: "study",
-    icon: "../experience/froggy.png",
-    role: "Explorer",
-    organization: "Career Break",
-    time: "03.2023 - now",
-    summary: "",
-    bullets: [
-      "Web Development Program @ Devhaus Leipzig",
-      "Fellow Scholar @ SheFi",
-      "Havard CS50: Introduction to Computer Science",
-    ],
-  },
-  {
-    type: "work",
-    icon: "../experience/qdb.svg",
-    role: "Product Manager",
-    organization: "QuestDB",
-    time: "09.2021 - 01.2023",
-    summary:
-      "QuestDB (YC S20) is a high-performance open source database for time series and its use cases cover financial trading, IIoT, renewable energy, and fleet tracking.",
-    bullets: [],
-  },
-  {
-    type: "work",
-    icon: "../experience/jina.svg",
-    role: "Product Manager, Developer Relations",
-    organization: "Jina AI",
-    time: "09.2020 - 09.2021",
-    summary:
-      "Jina AI is an open source software company that provides MLOps platform for multimodal AI, neural search and generative AI.",
-    bullets: [],
-  },
-  {
-    type: "study",
-    icon: "../experience/eth.svg",
-    role: "Devcon Scholar",
-    organization: "Ethereum Foundation",
-    time: "02.2019 - 10.2019",
-    summary:
-      "Selected as a Devcon V Scholar amongst 50 other candidates from around the world to participate in the Ethereum Foundation’s blockchain and cryptocurrency programming.",
-    bullets: [],
-  },
-  {
-    type: "work",
-    icon: "../experience/cloud_heat.svg",
-    role: "Business Development",
-    organization: "Cloud&Heat Technologies",
-    time: "08.2018 - 08.2020",
-    summary:
-      "Cloud&Heat provides managed Kubernetes, managed OpenStack, and sustainable datacenter solutions.",
-    bullets: [],
-  },
-  {
-    type: "work",
-    icon: "../experience/solvemate.svg",
-    role: "Product Management",
-    organization: "Solvemate",
-    time: "01.2017 - 03.2017",
-    summary:
-      "Cloud&Heat provides managed Kubernetes, managed OpenStack, and sustainable datacenter solutions.",
-    bullets: [],
-  },
-  {
-    type: "study",
-    icon: "../experience/hhl.svg",
-    role: "MSc in Management",
-    organization: "HHL Leipzig Graduate School of Management",
-    time: "09.2015 - 11.2017",
-    summary: "",
-    bullets: ["Marketing", "Innovation Management", "Economics & Regulation"],
-  },
-  {
-    type: "study",
-    icon: "../experience/snu.png",
-    role: "Global MBA",
-    organization: "Seoul National University",
-    time: "09.2016 - 12.2016",
-    summary: "Portfolios Management",
-    bullets: [],
-  },
-  {
-    type: "work",
-    icon: "../experience/yahoo.svg",
-    role: "Senior Financial Analyst",
-    organization: "Yahoo!",
-    time: "11.2011 - 12.2014",
-    summary:
-      "My responsibilities covers forecasting & reporting, revenue & cost analysis, audience metrics, PR/PO management, reorganization project management, etc.",
-    bullets: [],
-  },
-  {
-    type: "study",
-    icon: "../experience/ntpu.svg",
-    role: "BBA",
-    organization: "National Taipei University",
-    time: "09.2006 - 06.2010",
-    summary: "Finance and Cooperative Management",
-    bullets: [],
-  },
-  {
-    type: "tech",
-    header: "Technologies",
-    icon: "",
-    role: "",
-    organization: "",
-    time: "",
-    summary: "",
-    bullets: ["HTML", "CSS", "Git", "JavaScript", "SQL", "Command line"],
-  },
-  {
-    type: "tech",
-    header: "Tools",
-    icon: "",
-    role: "",
-    organization: "",
-    time: "",
-    summary: "",
-    bullets: ["Figma", "Productboard", "GitHub", "Shortcut", "Jira"],
-  },
-]
+type Exp = {
+  type: string;
+  header?: string;
+  icon: string;
+  role: string;
+  organization: string;
+  time: string;
+  summary: string;
+  bullets: string[];
+}
 
 export function About() {
-  const [experences, setExperiences] = useState(allExperiences)
+  const allExperiences = useLoaderData() as Exp[]
+  const [experiences, setExperiences] = useState(allExperiences)
   return (
     <div>
       <Summary />
@@ -142,7 +32,7 @@ export function About() {
         }}
       />
       <div className="flex flex-col gap-4">
-        {experences.map((exp, idx) => (
+        {experiences.map((exp, idx) => (
           <Experience
             key={idx}
             type={exp.type}
