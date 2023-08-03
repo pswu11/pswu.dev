27aea7644fc4f74a7a6304b2a8c5bbe83017ae91
Pei <pswu@protonmail.com>
2023-08-03 20:01:25 +0200

    img link

--- app/LogoMix.tsx
+++ app/LogoMix.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link"
-import Image from "next/image"
 
 function LogoMix() {
   return (
     <div className="flex h-7">
       <Link href="/" className="h-full">
-        <Image id="icon" src="pswu.svg" alt="pswu icon" />
+        <img id="icon" src="pswu.svg" alt="pswu icon"/>
       </Link>
       <div className="blinking-cursor h-full"></div>
     </div>
