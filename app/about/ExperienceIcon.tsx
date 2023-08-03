d99ee955127a7c5e77ab9ecd45dd9338c10f3632
Pei <pswu@protonmail.com>
2023-08-03 20:05:19 +0200

    fix img

--- app/about/ExperienceIcon.tsx
+++ app/about/ExperienceIcon.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image"
-
 type Props = {
   icon: string
   alt: string
@@ -8,7 +6,7 @@ type Props = {
 function ExperienceIcon({icon, alt}: Props) {
   return (
     <div id="exp-icon" className="w-12 h-12 aspect-square flex justify-center items-center bg-base-darkpurple rounded-xl">
-      <Image src={icon} alt={alt} className="h-3/5 aspect-square"/>
+      <img src={icon} alt={alt} className="h-3/5 aspect-square"/>
     </div>
   )
 }
--- app/about/ProfilePic.tsx
+++ app/about/ProfilePic.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image"
-
 function ProfilePic() {
   return (
     <div id="summary-pic" className="w-20 h-20 aspect-square">
-      <Image className="w-full object-cover object-center rounded-full " src="pswu002.png" alt="cartoon-profile-pic" />
+      <img className="w-full object-cover object-center rounded-full " src="pswu002.png" alt="cartoon-profile-pic" />
     </div>
   )
 }
