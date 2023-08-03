e7f3095885cb6f70e6195c2a1bd53cb688fd1837
Pei <pswu@protonmail.com>
2023-08-03 20:36:28 +0200

    fix path

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,14 +1,14 @@
 import fs from "fs"
 import matter from "gray-matter"
 import { MDXRemote } from "next-mdx-remote/rsc"
+import path from 'path';
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const fileName = `${params.slug}.md`
-  const markdown = fs.readFileSync(`posts/${fileName}`, "utf-8")
+  const filePath = `${path.join(process.cwd(), 'posts')}/${params.slug}.md`
+  const markdown = fs.readFileSync(filePath, "utf-8")
   const { data: frontMatter, content } = matter(markdown)
   return (
     <article className="prose prose-invert prose-h2:text-2xl prose-h2:font-bold">
-      <p>file name {fileName}</p>
       <h2>{frontMatter.title}</h2>
       <MDXRemote source={content} />
     </article>
