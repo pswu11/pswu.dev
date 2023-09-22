4bc8ab1262e5a9375863306a3f83f075e2aa05c7
Pei <pswu@protonmail.com>
2023-09-22 15:17:04 +0200

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="experience/froggy.png" sizes="any" />
+      </head>
       <body className={`${inter.className} gradient-purple text-base-white`}>
         <div
           id="wrapper"
